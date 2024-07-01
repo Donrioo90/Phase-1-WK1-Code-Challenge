880e906e1424610e4b7650cead8f517cca4f9c2f
wanjala <ranjala90@gmail.com>
2024-07-01 14:45:35 +0300

    Toy problems Prerequisites

---
+++ student-grade-generator.js
@@ -0,0 +1,18 @@
+let studentScore = 80;
+let studentName = 'Ronnie';
+
+if (studentScore >79) {
+    console.log (`Congratulations, ${studentName}, you scored ${studentScore}. You got an A on this test.`);
+} else if (studentScore >= 60 && studentScore < 79) {
+    console.log (`Congratulations, ${studentName}, you scored ${studentScore}. You got an B on this test.`);
+}
+else if (studentScore >= 49 && studentScore < 59) {
+    console.log(`Congratulations, ${studentName}, you scored ${studentScore}. You got a C on this test.`);
+}
+else if (studentScore >=40 && studentScore < 49){
+console.log(`hi, ${studentName}, you scored ${studentScore}. You got a D on this test.`);
+}
+else if (studentScore < 40){
+   console.log(`hi, ${studentName}, you scored ${studentScore}. You got an E on this test.`);
+   }
+
